94808e87d738f203916b1c71994108a8fc4a505f
i-ght <toph.ght@gmail.com>
2023-12-11 14:14:39 -0700

    animate

--- codex.js
+++ codex.js
@@ -1,31 +1,20 @@
-
-function getRandomArbitrary(min, max) {
-    return Math.round(Math.random() * (max - min) + min);
-  }  
-
-function getRollInterval() {
-    return `0s`;
-}
-
-function getReadInterval() {
-    return `${getRandomArbitrary(1333, 1999)}s`;
-}
-
-let phase = "rolling";
-/* change(); */
+let phase = "running";
 
 function change() {
-    let interval;
-    if (phase === "rolling") {
-        phase = "reading";
-        interval = getReadInterval();
-        setTimeout(change, 3333); 
-    } else if (phase === "reading") {
-        phase = "rolling";
-        interval = getRollInterval();
-        setTimeout(change, 333); 
-    }
+    
+    if (phase === "running") {
+        phase = "paused";
+        setTimeout(change, 9999);
+    } else if (phase == "paused") {
+        phase = "running";
+        setTimeout(change, 1300);
+    } else { throw null; }
 
     const style = document.documentElement.style;
-    style.setProperty("--marquee_duration", interval);
+    style.setProperty(
+        "--marquee_play_state",
+        phase
+    );
 }
+
+setTimeout(change, 2222);
