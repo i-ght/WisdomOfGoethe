20be5c0a24858e711f738dc7921f86c51bcb982f
i-ght <toph.ght@gmail.com>
2023-12-13 15:42:23 -0700

    fade away then back

--- codex.js
+++ codex.js
@@ -1,20 +1,59 @@
 let phase = "running";
 
-function change() {
-    
-    if (phase === "running") {
-        phase = "paused";
-        setTimeout(change, 9999);
-    } else if (phase == "paused") {
-        phase = "running";
-        setTimeout(change, 1300);
-    } else { throw null; }
-
-    const style = document.documentElement.style;
-    style.setProperty(
-        "--marquee_play_state",
-        phase
-    );
+function getQuotes() {
+    const htmlQuotes = document.getElementById("quotes");
+    const lis = htmlQuotes.getElementsByTagName("li");
+    const quotes = [];
+    for (const li of lis) {
+        const p = li.getElementsByTagName("p")[0];
+        quotes.push(p.textContent);
+    }
+    return quotes;
 }
 
-setTimeout(change, 2222);
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        // Swap array[i] and array[j]
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function construct3QuotesHtmlList(one, two, three) {
+    return `<ul><li>${one}</li><li>${two}</li><li>${three}</li></ul>`
+}
+
+function dequeueThreeQuotes(quotes) {
+    if (quotes.length < 3) {
+        throw new Error("quotes queue length needs to be > 3.");
+    }
+
+    return [quotes.shift(), quotes.shift(), quotes.shift()];
+}
+
+function changeReflectorContents(quotes) {
+    const [one, two, three] = dequeueThreeQuotes(quotes);
+    const reflector = document.getElementById("reflector");
+    reflector.innerHTML = construct3QuotesHtmlList(one, two, three);
+}
+
+function initReflector(quotes) {
+    const reflector = document.getElementById("reflector");
+    reflector.addEventListener("animationiteration", () => {
+        if (quotes.length < 3) {
+            const newQuotes = getQuotes();
+            shuffleArray(newQuotes);
+            quotes.push(...newQuotes)
+        }
+        changeReflectorContents(quotes);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const quotes = getQuotes();
+    shuffleArray(quotes);
+    
+    initReflector(quotes);
+    changeReflectorContents(quotes);
+});
+
